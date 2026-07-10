174923bee8b1ac55b9bd3de489c59eba3325afb0
Gleb Kravchuk <gvkravchuk@edu.hse.ru>
2026-07-10 20:28:37 +0300

    Добавление репозитория

---
+++ src/main/java/net/devedemon/testmod/TestMod.java
@@ -0,0 +1,21 @@
+package net.devedemon.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
